46303f0847d0a94cc7048a578cda9e64a0d7271e
sharif-jobayed <sharif.jobayed@gmail.com>
2024-07-15 09:12:12 +0600

    Created performPost() util

--- src/test/java/RESTAssured/AirlineTest.java
+++ src/test/java/RESTAssured/AirlineTest.java
@@ -1,29 +1,29 @@
 package RESTAssured;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
+import static utils.RESTUtils.performPost;
+
 public class AirlineTest {
     @Test()
     void createAirline() {
-        Response response = RestAssured.given().log().all()
-                .baseUri("https://api.instantwebtools.net/v1/airlines")
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
-                        "    \"name\": \"Sri Lankan Airways\",\n" +
-                        "    \"country\": \"Sri Lanka\",\n" +
-                        "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
-                        "    \"slogan\": \"From Sri Lanka\",\n" +
-                        "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
-                        "    \"website\": \"www.srilankaaairways.com\",\n" +
-                        "    \"established\": \"1990\"\n" +
-                        "}")
-                .post()
-                .then().log().all().extract().response();
+        String endpoint = "https://api.instantwebtools.net/v1/airlines";
+        String payload = "{\n" +
+                "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
+                "    \"name\": \"Sri Lankan Airways\",\n" +
+                "    \"country\": \"Sri Lanka\",\n" +
+                "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
+                "    \"slogan\": \"From Sri Lanka\",\n" +
+                "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
+                "    \"website\": \"www.srilankaaairways.com\",\n" +
+                "    \"established\": \"1990\"\n" +
+                "}";
+
+       Response response = performPost(endpoint, payload, new HashMap<>());
         Assert.assertEquals(response.statusCode(), 200, "The request wasn't successful");
     }
 }
---
+++ src/test/java/utils/RESTUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+public class RESTUtils {
+    public static Response performPost(String endpoint, String requestPayload, Map<String, String> headers) {
+        return RestAssured.given().log().all()
+                .baseUri(endpoint)
+                .headers(headers)
+                .contentType(ContentType.JSON)
+                .body(requestPayload)
+                .post()
+                .then().log().all().extract().response();
+    }
+}
