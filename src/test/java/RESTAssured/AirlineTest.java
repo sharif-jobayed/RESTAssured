7d7cad7af2d0d4f6a8d6148156b85b7bda1c36e2
sharif-jobayed <sharif.jobayed@gmail.com>
2024-07-15 09:18:45 +0600

    No major changes

--- src/test/java/RESTAssured/AirlineTest.java
+++ src/test/java/RESTAssured/AirlineTest.java
@@ -12,16 +12,17 @@ public class AirlineTest {
     @Test()
     void createAirline() {
         String endpoint = "https://api.instantwebtools.net/v1/airlines";
-        String payload = "{\n" +
-                "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
-                "    \"name\": \"Sri Lankan Airways\",\n" +
-                "    \"country\": \"Sri Lanka\",\n" +
-                "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
-                "    \"slogan\": \"From Sri Lanka\",\n" +
-                "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
-                "    \"website\": \"www.srilankaaairways.com\",\n" +
-                "    \"established\": \"1990\"\n" +
-                "}";
+        String payload = """
+                {
+                    "_id":"252d3bca-d9bb-476c-9a97-562d685e235c",
+                    "name": "Sri Lankan Airways",
+                    "country": "Sri Lanka",
+                    "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+                    "slogan": "From Sri Lanka",
+                    "head_quaters": "Katunayake, Sri Lanka",
+                    "website": "www.srilankaaairways.com",
+                    "established": "1990"
+                }""";
 
        Response response = performPost(endpoint, payload, new HashMap<>());
         Assert.assertEquals(response.statusCode(), 200, "The request wasn't successful");
